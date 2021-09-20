fd327f386972a1bf70f15d6b964e84e4ff0909de
Aravindrajan_Sampath <aravindrajan_sampath@dellteam.com>
2021-09-20 08:35:20 +0530

    Error Fix

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
@@ -7,11 +7,30 @@ import classes from './HeaderCartButton.module.css'
 
 
 const HeaderCartButton = props => {
-    const cartCts = useContext(CartContext);
-    const numberOfCartItems = cartCts.items.reduce((curNumber, item) => { 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
-    return <button className={classes.button} onClick={props.onClick}>
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,7 +9,7 @@ const defaultCartState = {
 
 const cardReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedTotalAmount = state.totalAmount + action.item.price + action.item.amount;
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
         const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
         const existingCartItem = state.items[existingCartItemIndex];
 
